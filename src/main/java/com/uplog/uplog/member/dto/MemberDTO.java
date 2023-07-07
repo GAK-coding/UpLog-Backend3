7ae8f0c03c304844a0a9be2b6be3548e575d9b7c
yunjjung <yjung203@naver.com>
2023-07-07 17:22:16 +0900

    [ADD]Member DTO 추가

--- src/main/java/com/uplog/uplog/member/dto/MemberDTO.java
+++ src/main/java/com/uplog/uplog/member/dto/MemberDTO.java
@@ -1,4 +1,52 @@
 package com.uplog.uplog.member.dto;
 
+import com.uplog.uplog.member.model.LoginType;
+import com.uplog.uplog.member.model.Member;
+import com.uplog.uplog.member.model.MemberStatus;
+import com.uplog.uplog.member.model.Position;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class MemberDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SaveMemberRequest{
+        private String email;
+        private String name;
+        private String nickname;
+        private String password;
+        private Position position;
+        private LoginType loginType;
+        private MemberStatus memberStatus;
+
+        public Member toMemberEntity(){
+            return Member.builder()
+                    .email(email)
+                    .name(name)
+                    .nickname(nickname)
+                    .password(password)
+                    .position(position)
+                    .loginType(loginType)
+                    .memberStatus(MemberStatus.MEMBER_ACTIVE)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MemberInfoDTO{
+        private Long Id;
+        private String email;
+        private String name;
+        private String nickname;
+        private String password;
+        private Position position;
+    }
 }
