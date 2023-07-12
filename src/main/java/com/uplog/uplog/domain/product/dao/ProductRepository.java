08339058d0062a28bce5a8d6b6618c59f98e8015
yunjjung <yjung203@naver.com>
2023-07-13 06:34:42 +0900

    [REFACTOR]Product 수정

--- src/main/java/com/uplog/uplog/domain/product/dao/ProductRepository.java
+++ src/main/java/com/uplog/uplog/domain/product/dao/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.uplog.uplog.domain.product.dao;
 
+import com.uplog.uplog.domain.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
