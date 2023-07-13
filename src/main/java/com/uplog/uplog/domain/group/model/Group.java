ba253303a598851070db6d07204da007f1127cb1
yunjjung <yjung203@naver.com>
2023-07-13 15:49:02 +0900

    [REFACTOR]erd수정으로 인한 그룹 수정

--- src/main/java/com/uplog/uplog/domain/group/model/Group.java
+++ src/main/java/com/uplog/uplog/domain/group/model/Group.java
@@ -1,20 +1,18 @@
 package com.uplog.uplog.domain.group.model;
 
-import com.uplog.uplog.domain.member.model.Member;
 import com.uplog.uplog.global.BaseTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED)
-public class GroupBase extends BaseTime {
+public class Group extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "group_id")
