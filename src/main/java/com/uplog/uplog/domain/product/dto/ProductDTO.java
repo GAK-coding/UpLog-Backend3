829a9bf53715cf1a9a012507e0e5315c2ac93499
eunryeong1 <lyungeg@gmail.com>
2023-07-14 15:39:16 +0900

    [ADD] prodcut create controller

--- src/main/java/com/uplog/uplog/domain/product/dto/ProductDTO.java
+++ src/main/java/com/uplog/uplog/domain/product/dto/ProductDTO.java
@@ -1,4 +1,28 @@
 package com.uplog.uplog.domain.product.dto;
 
+import com.uplog.uplog.domain.project.dto.ProjectDTO;
+import com.uplog.uplog.domain.team.model.Team;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 public class ProductDTO {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SaveProductRequest{
+        private Long id;
+        private Team team;
+        private String company;
+    }
+
+
+
+
+
+
 }
