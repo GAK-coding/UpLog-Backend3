18bb9e8b04850bcc6801e7c6afd9d5e4a04d612d
yunjjung <yjung203@naver.com>
2023-07-14 13:14:59 +0900

    [ADD]Task mapping

--- src/main/java/com/uplog/uplog/domain/task/model/Task.java
+++ src/main/java/com/uplog/uplog/domain/task/model/Task.java
@@ -1,11 +1,14 @@
 package com.uplog.uplog.domain.task.model;
 
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.menu.model.Menu;
 import com.uplog.uplog.global.BaseTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -16,4 +19,21 @@ public class Task extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "task_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member targetMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    private TaskStatus taskStatus;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+
+
+
+
 }
