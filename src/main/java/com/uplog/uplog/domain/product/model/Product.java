157a7f0076aaa64f26012dab0dd6c489187b55f5
yunjjung <yjung203@naver.com>
2023-07-14 13:16:04 +0900

    [ADD]product mapping

--- src/main/java/com/uplog/uplog/domain/product/model/Product.java
+++ src/main/java/com/uplog/uplog/domain/product/model/Product.java
@@ -1,5 +1,6 @@
 package com.uplog.uplog.domain.product.model;
 
+import com.uplog.uplog.domain.team.model.Team;
 import com.uplog.uplog.global.BaseTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,4 +17,11 @@ public class Product extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    private String company;
+
 }
