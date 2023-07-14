e9334ddce325f6ff49f93eff253ce759e151de48
eunryeong1 <lyungeg@gmail.com>
2023-07-14 16:34:46 +0900

    [ADD] prodcut create controller

--- src/main/java/com/uplog/uplog/domain/product/application/ProductService.java
+++ src/main/java/com/uplog/uplog/domain/product/application/ProductService.java
@@ -21,9 +21,17 @@ public class ProductService {
     private final ProjectRepository projectRepository;
 
 
-//    public ProductDTO createProduct(ProductDTO productDTO) {
-//
-//    }
+    //기업이 처음 제품 생성할때
+    public ProductDTO createProduct(ProductDTO productDTO) {
+
+
+    }
+
+    //마스터, 리더들이 제품에 멤버 추가할때
+
+
+
+    //제품수정(이름,이미지,의뢰인추가->의뢰인추가는 팀에서 관리해야하는거같기도?)
 
 
 
--- src/main/java/com/uplog/uplog/domain/product/dto/ProductDTO.java
+++ src/main/java/com/uplog/uplog/domain/product/dto/ProductDTO.java
@@ -1,5 +1,7 @@
 package com.uplog.uplog.domain.product.dto;
 
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.product.model.Product;
 import com.uplog.uplog.domain.project.dto.ProjectDTO;
 import com.uplog.uplog.domain.team.model.Team;
 import lombok.AllArgsConstructor;
@@ -18,6 +20,14 @@ public class ProductDTO {
         private Long id;
         private Team team;
         private String company;
+
+        public Product toProductEntity(){
+            return Product.builder()
+                    .id(id)
+                    .team(team)
+                    .company(company)
+                    .build();
+        }
     }
 
 
