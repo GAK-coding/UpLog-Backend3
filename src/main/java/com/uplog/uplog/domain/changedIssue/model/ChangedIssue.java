ad99a1af80c8244ae4eabf3d873809e6c82fe90c
yunjjung <yjung203@naver.com>
2023-07-17 20:16:15 +0900

    [ADD]ChangedIssue mapping and setting

--- src/main/java/com/uplog/uplog/domain/changedIssue/model/ChangedIssue.java
+++ src/main/java/com/uplog/uplog/domain/changedIssue/model/ChangedIssue.java
@@ -1,5 +1,7 @@
 package com.uplog.uplog.domain.changedIssue.model;
 
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.project.model.Project;
 import com.uplog.uplog.global.BaseTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,4 +18,20 @@ public class ChangedIssue extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "changedIssue_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @Enumerated(EnumType.STRING)
+    private IssueStatus issueStatus;
+
+    private String title;
+    private String content;
+
+
 }
