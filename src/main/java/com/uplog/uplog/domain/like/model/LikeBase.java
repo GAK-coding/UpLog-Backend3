0e9f0476afae6a97dd02448ada6ed2b54b8f9a3e
yunjjung <yjung203@naver.com>
2023-07-17 20:17:55 +0900

    [ADD]like mapping and setting

---
+++ src/main/java/com/uplog/uplog/domain/like/model/LikeBase.java
@@ -0,0 +1,28 @@
+package com.uplog.uplog.domain.like.model;
+
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.post.model.Post;
+import com.uplog.uplog.global.BaseTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public class LikeBase extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
---
+++ src/main/java/com/uplog/uplog/domain/like/model/LikeType.java
@@ -0,0 +1,5 @@
+package com.uplog.uplog.domain.like.model;
+
+public enum LikeType {
+    LIKE_POST, LIKE_COMMENT;
+}
