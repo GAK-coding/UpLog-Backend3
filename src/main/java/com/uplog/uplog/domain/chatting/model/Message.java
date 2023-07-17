c8f2634db6b80cab97e88a9f58677eeeb737e0a5
yunjjung <yjung203@naver.com>
2023-07-17 20:21:02 +0900

    [ADD]postTag mapping and setting

---
+++ src/main/java/com/uplog/uplog/domain/chatting/model/Message.java
@@ -0,0 +1,14 @@
+package com.uplog.uplog.domain.chatting.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+}
