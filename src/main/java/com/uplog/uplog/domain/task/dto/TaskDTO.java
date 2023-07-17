08075166daf47952209647922f265d3bfc27cc75
eunryeong1 <lyungeg@gmail.com>
2023-07-17 16:10:35 +0900

    [ADD] task dto

--- src/main/java/com/uplog/uplog/domain/task/dto/TaskDTO.java
+++ src/main/java/com/uplog/uplog/domain/task/dto/TaskDTO.java
@@ -1,4 +1,108 @@
 package com.uplog.uplog.domain.task.dto;
 
+import com.uplog.uplog.domain.member.dto.MemberDTO;
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.task.model.Task;
+import com.uplog.uplog.domain.task.model.TaskStatus;
+import com.uplog.uplog.domain.menu.model.Menu;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class TaskDTO {
+    private Long id;
+    private MemberDTO targetMember;
+    private TaskStatus taskStatus;
+    private String taskDetail;
+    private LocalDate startTime;
+    private LocalDate endTime;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TaskSaveRequest {
+        private Member targetMember;
+        private Menu menu;
+        private TaskStatus taskStatus;
+        private LocalDate startTime;
+        private LocalDate endTime;
+
+        public Task toEntity() {
+            return Task.builder()
+                    .targetMember(targetMember)
+                    .menu(menu)
+                    .taskStatus(taskStatus)
+                    .startTime(startTime)
+                    .endTime(endTime)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TaskInfoDTO{
+        private Long id;
+        private Member targetmember;
+        private Menu menu;
+        private TaskStatus taskStatus;
+        private String taskDetail;
+        private LocalDate startTime;
+        private LocalDate endTime;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateTaskRequest{
+        private Long id;
+        private Member targetmember;
+        private Menu menu;
+        private TaskStatus taskStatus;
+        private String taskDetail;
+        private LocalDate startTime;
+        private LocalDate endTime;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateTaskStatusRequest{
+        private Long id;
+        private TaskStatus taskStatus;
+
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateTaskDTO{
+        private Long id;
+        private Member targetmember;
+        private Menu menu;
+        private TaskStatus taskStatus;
+        private String taskDetail;
+        private LocalDate startTime;
+        private LocalDate endTime;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class updateTaskStatusDTO{
+        private Long id;
+        private TaskStatus taskStatus;
+    }
+
+
 }
--- src/main/java/com/uplog/uplog/domain/task/model/Task.java
+++ src/main/java/com/uplog/uplog/domain/task/model/Task.java
@@ -2,16 +2,19 @@ package com.uplog.uplog.domain.task.model;
 
 import com.uplog.uplog.domain.member.model.Member;
 import com.uplog.uplog.domain.menu.model.Menu;
+import com.uplog.uplog.domain.task.dto.TaskDTO.*;
 import com.uplog.uplog.global.BaseTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task extends BaseTime {
@@ -29,9 +32,38 @@ public class Task extends BaseTime {
     private Menu menu;
 
     private TaskStatus taskStatus;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private String taskDetail;
+    private LocalDate startTime;
+    private LocalDate endTime;
 
+
+    public TaskInfoDTO toTaskInfoDTO(){
+        return TaskInfoDTO.builder()
+                .id(this.getId())
+                .targetmember(this.getTargetMember())
+                .menu(this.getMenu())
+                .taskStatus(this.getTaskStatus())
+                .startTime(this.getStartTime())
+                .endTime(this.getEndTime())
+                .build();
+    }
+
+    public UpdateTaskDTO toUpdateTaskDTO(UpdateTaskRequest updateTaskRequest){
+        return UpdateTaskDTO.builder()
+                .id(this.getId())
+                .targetmember(this.getTargetMember())
+                .menu(this.getMenu())
+                .taskStatus(this.getTaskStatus())
+                .startTime(this.getStartTime())
+                .endTime(this.getEndTime())
+                .build();
+    }
+    public updateTaskStatusDTO toUpdateTaskStatusDTO (UpdateTaskStatusRequest updateTaskStatusRequest){
+        return updateTaskStatusDTO.builder()
+                .id(this.getId())
+                .taskStatus(this.getTaskStatus())
+                .build();
+    }
 
 
 
