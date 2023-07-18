144d28bc8cdf7a077771aa09ccd24508e50c86d7
eunryeong1 <lyungeg@gmail.com>
2023-07-18 10:57:23 +0900

    [ADD] task exception

--- src/main/java/com/uplog/uplog/domain/task/exception/handler/NotFoundTaskByIdException.java
+++ src/main/java/com/uplog/uplog/domain/task/exception/handler/NotFoundTaskByIdException.java
@@ -1,11 +1,6 @@
 package com.uplog.uplog.domain.task.exception.handler;
 
-public class NotFoundTaskByIdException extends Exception {
-    public NotFoundTaskByIdException(String m) {
-        super(m);
-    }
-
-    public NotFoundTaskByIdException() {
-        super("해당 id를 가진 task는 존재하지 않습니다");
-    }
+public class NotFoundTaskByIdException extends IllegalArgumentException{
+    public NotFoundTaskByIdException(String m){ super(m);}
+    public NotFoundTaskByIdException(){super("해당 id를 가진 task는 존재하지 않습니다");}
 }
