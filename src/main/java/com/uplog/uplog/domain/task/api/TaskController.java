0717f7fe4529eb97e138815b41f4433641d4d555
eunryeong1 <lyungeg@gmail.com>
2023-07-18 11:07:27 +0900

    [Refactor] task service

--- src/main/java/com/uplog/uplog/domain/task/api/TaskController.java
+++ src/main/java/com/uplog/uplog/domain/task/api/TaskController.java
@@ -1,50 +1,11 @@
 package com.uplog.uplog.domain.task.api;
 
-import com.uplog.uplog.domain.task.application.TaskService;
-import com.uplog.uplog.domain.task.dto.TaskDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class TaskController {
-
-    private final TaskService taskService;
-
-//    @PostMapping
-//    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
-//        TaskDTO createdTask = taskService.createTask(taskDTO);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
-//    }
-//
-//    @GetMapping("/{taskId}")
-//    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long taskId) {
-//        TaskDTO taskDTO = taskService.getTaskById(taskId);
-//        return ResponseEntity.ok(taskDTO);
-//    }
-//
-//    @PutMapping("/{taskId}")
-//    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long taskId, @RequestBody TaskDTO taskDTO) {
-//        TaskDTO updatedTask = taskService.updateTask(taskId, taskDTO);
-//        return ResponseEntity.ok(updatedTask);
-//    }
-//
-//    @DeleteMapping("/{taskId}")
-//    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
-//        taskService.deleteTask(taskId);
-//        return ResponseEntity.noContent().build();
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<TaskDTO>> getAllTasks() {
-//        List<TaskDTO> tasks = taskService.getAllTasks();
-//        return ResponseEntity.ok(tasks);
-//    }
 }
-
--- src/main/java/com/uplog/uplog/domain/task/application/TaskService.java
+++ src/main/java/com/uplog/uplog/domain/task/application/TaskService.java
@@ -7,11 +7,9 @@ import com.uplog.uplog.domain.task.exception.handler.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.webjars.NotFoundException;
 
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
