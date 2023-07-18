289edc1d86e1591ab31c88ad186d30ff05376122
yunjjung <yjung203@naver.com>
2023-07-18 21:09:24 +0900

    [ADD]이메일 관련 추가

---
+++ src/main/java/com/uplog/uplog/global/config/MailConfig.java
@@ -0,0 +1,38 @@
+package com.uplog.uplog.global.config;
+
+import com.uplog.uplog.global.method.GlobalMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.naver.com"); //smtp 서버 주소(메인 도메인 서버 주소)
+        javaMailSender.setUsername("gak_uplog"); //naver id
+        javaMailSender.setPassword("rkrdmldjqfhrm");//naver password
+
+        javaMailSender.setPort(465); //메일 인증서버 포트
+
+        javaMailSender.setJavaMailProperties(getMailProperties()); //메일 인증서버 정보 가져오기
+
+        return javaMailSender;
+    }
+    private Properties getMailProperties(){
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp"); //프로토콜 설정
+        properties.setProperty("mail.smtp.auth", "true"); //smtp 인증
+        properties.setProperty("mail.smtp.starttls.enalbe", "true"); //smtp stattles 사용
+        properties.setProperty("mail.debug", "true"); //debug 사용
+        properties.setProperty("mail.smtp.ssl.turst", "smtp.naver.com");//ssl인증 서버는 smtp.naver.com
+        properties.setProperty("mail.smtp.ssl.enable", "true");//ssl사용
+
+        return properties;
+    }
+}
