30570ff75e77b676194a747bdc68a14ae6464336
yunjjung <yjung203@naver.com>
2023-07-23 15:52:51 +0900

    [ADD]Team 생성 추가_1

--- src/main/java/com/uplog/uplog/domain/team/api/TeamController.java
+++ src/main/java/com/uplog/uplog/domain/team/api/TeamController.java
@@ -8,4 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class TeamController {
+    //team 생성시 기업만 만들 수 있으므로 pathvariable에 기업의 아이디가 들어감.
 }
