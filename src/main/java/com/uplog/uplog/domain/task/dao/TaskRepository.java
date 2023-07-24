6b666463d60b7bfd6655622b5c3fc2a647e25b95
eunryeong1 <lyungeg@gmail.com>
2023-07-24 20:08:20 +0900

    [REFACTOR] task 추가

--- src/main/java/com/uplog/uplog/domain/task/dao/TaskRepository.java
+++ src/main/java/com/uplog/uplog/domain/task/dao/TaskRepository.java
@@ -4,9 +4,11 @@ import com.uplog.uplog.domain.task.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findById(Long id);
+    List<Task> findByTeamId(Long TeamId);
 }
