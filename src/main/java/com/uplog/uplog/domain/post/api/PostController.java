b3c31e4158a8e0b30995c91ac853e1eb5441fac1
eunryeong1 <lyungeg@gmail.com>
2023-07-26 16:12:45 +0900

    [ADD] Post update

--- src/main/java/com/uplog/uplog/domain/post/api/PostController.java
+++ src/main/java/com/uplog/uplog/domain/post/api/PostController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class PostController {
     private final PostService postService;
+
+    //create
     @PostMapping(value="/posts/{member_id}")
     public ResponseEntity<PostInfoDTO> createPost(@PathVariable(name = "member_id") Long id, @RequestBody CreatePostRequest createPostRequest) {
         Post createdPost = postService.createPost(id,createPostRequest);
@@ -20,11 +22,46 @@ public class PostController {
         return ResponseEntity.ok(postInfoDTO);
     }
 
+
+    //delete
     @DeleteMapping("posts/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id){
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
     }
+
+    //update
+    @PatchMapping("/posts/{post_id}/title")
+    public ResponseEntity<PostInfoDTO> updatePostTitle(@PathVariable Long id,@RequestBody UpdatePostTitleRequest updatePostTitleRequest){
+        Post updatedPost=postService.updatePostTitle(id,updatePostTitleRequest);
+        PostInfoDTO postInfoDTO=updatedPost.toPostInfoDTO();
+        return ResponseEntity.ok(postInfoDTO);
+    }
+
+    @PatchMapping("/posts/{post_id}/content")
+    public ResponseEntity<PostInfoDTO> updatePostContent(@PathVariable Long id,@RequestBody UpdatePostContentRequest updatePostContentRequest){
+        Post updatedPost=postService.updatePostContent(id,updatePostContentRequest);
+        PostInfoDTO postInfoDTO=updatedPost.toPostInfoDTO();
+        return ResponseEntity.ok(postInfoDTO);
+    }
+
+    @PatchMapping("/posts/{post_id}/type")
+    public ResponseEntity<PostInfoDTO> updatePostType(@PathVariable Long id,@RequestBody UpdatePostTypeRequest updatePostTypeRequest){
+        Post updatedPost=postService.updatePostType(id,updatePostTypeRequest);
+        PostInfoDTO postInfoDTO=updatedPost.toPostInfoDTO();
+        return ResponseEntity.ok(postInfoDTO);
+    }
+    @PatchMapping("/posts/{post_id}/menu")
+    public ResponseEntity<PostInfoDTO> updatePostMenu(@PathVariable Long id,@RequestBody UpdatePostMenuRequest updatePostMenuRequest){
+        Post updatedPost=postService.updatePostMenu(id,updatePostMenuRequest);
+        PostInfoDTO postInfoDTO=updatedPost.toPostInfoDTO();
+        return ResponseEntity.ok(postInfoDTO);
+    }
+
+
+
+
+
 
 
 }
