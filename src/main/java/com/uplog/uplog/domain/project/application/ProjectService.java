5fb074a99e38c766c5e7b83130b1317dc2fa8ae7
june <junnim123@naver.com>
2023-07-27 12:55:48 +0900

    feat : project create

--- src/main/java/com/uplog/uplog/domain/project/application/ProjectService.java
+++ src/main/java/com/uplog/uplog/domain/project/application/ProjectService.java
@@ -1,11 +1,29 @@
 package com.uplog.uplog.domain.project.application;
 
+import com.uplog.uplog.domain.project.dto.ProjectDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static com.uplog.uplog.domain.project.dto.ProjectDTO.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProjectService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Transactional
+    public CreateInitInfo createInit(CreateInitInfo createInitInfo, Long prodId){
+
+
+        return createInitInfo;
+    }
 }
--- src/main/java/com/uplog/uplog/domain/project/dto/ProjectDTO.java
+++ src/main/java/com/uplog/uplog/domain/project/dto/ProjectDTO.java
@@ -1,4 +1,31 @@
 package com.uplog.uplog.domain.project.dto;
 
+import com.uplog.uplog.domain.project.model.Project;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class ProjectDTO {
+
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreateInitInfo{
+
+
+        private Long id;
+        private String version;
+
+        public Project of(){
+            return Project.builder()
+                    .version(version)
+                    .build();
+        }
+
+    }
 }
