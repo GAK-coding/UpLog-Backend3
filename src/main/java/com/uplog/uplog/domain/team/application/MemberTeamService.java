eb7d0004d12bc3aed0690b48352ac3e9f1657246
yunjjung <yjung203@naver.com>
2023-07-31 15:22:09 +0900

    [REFACTOR]CRUD 변수명 수정

--- src/main/java/com/uplog/uplog/domain/team/application/MemberTeamService.java
+++ src/main/java/com/uplog/uplog/domain/team/application/MemberTeamService.java
@@ -3,17 +3,15 @@ package com.uplog.uplog.domain.team.application;
 import com.uplog.uplog.domain.member.dao.MemberRepository;
 import com.uplog.uplog.domain.member.dto.MemberDTO;
 import com.uplog.uplog.domain.member.dto.MemberDTO.PowerMemberInfoDTO;
-import com.uplog.uplog.domain.member.dto.MemberDTO.SaveMemberRequest;
 import com.uplog.uplog.domain.member.exception.NotFoundMemberByEmailException;
 import com.uplog.uplog.domain.member.model.Member;
 import com.uplog.uplog.domain.team.dao.MemberTeamRepository;
 import com.uplog.uplog.domain.team.dao.TeamRepository;
 import com.uplog.uplog.domain.team.dto.TeamDTO;
-import com.uplog.uplog.domain.team.dto.TeamDTO.SaveTeamRequest;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO;
+import com.uplog.uplog.domain.team.dto.memberTeamDTO.CreateMemberTeamRequest;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO.MemberPowerListDTO;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO.MemberTeamInfoDTO;
-import com.uplog.uplog.domain.team.dto.memberTeamDTO.SaveMemberTeamRequest;
 import com.uplog.uplog.domain.team.model.MemberTeam;
 import com.uplog.uplog.domain.team.model.Team;
 import com.uplog.uplog.global.exception.NotFoundIdException;
@@ -34,10 +32,10 @@ public class MemberTeamService {
     private final TeamRepository teamRepository;
 
     @Transactional
-    public MemberTeamInfoDTO saveMemberTeam(SaveMemberTeamRequest saveMemberTeamRequest){
-        Member member = memberRepository.findMemberByEmail(saveMemberTeamRequest.getMemberEmail()).orElseThrow(NotFoundMemberByEmailException::new);
-        Team team= teamRepository.findById(saveMemberTeamRequest.getTeamId()).orElseThrow(NotFoundIdException::new);
-        MemberTeam memberTeam = saveMemberTeamRequest.toMemberTeam(team, member, saveMemberTeamRequest.getPowerType());
+    public MemberTeamInfoDTO createMemberTeam(CreateMemberTeamRequest createMemberTeamRequest){
+        Member member = memberRepository.findMemberByEmail(createMemberTeamRequest.getMemberEmail()).orElseThrow(NotFoundMemberByEmailException::new);
+        Team team= teamRepository.findById(createMemberTeamRequest.getTeamId()).orElseThrow(NotFoundIdException::new);
+        MemberTeam memberTeam = createMemberTeamRequest.toMemberTeam(team, member, createMemberTeamRequest.getPowerType());
 
         memberTeamRepository.save(memberTeam);
         team.getMemberTeamList().add(memberTeam);
