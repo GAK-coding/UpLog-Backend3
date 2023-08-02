429dddb19d4afc0b790d4bad20b2b11eea601833
eunryeong1 <lyungeg@gmail.com>
2023-08-02 13:57:22 +0900

    [ADD] menu 임시 create

--- src/main/java/com/uplog/uplog/domain/menu/api/MenuController.java
+++ src/main/java/com/uplog/uplog/domain/menu/api/MenuController.java
@@ -1,11 +1,31 @@
 package com.uplog.uplog.domain.menu.api;
 
+import com.uplog.uplog.domain.menu.application.MenuService;
+import com.uplog.uplog.domain.menu.dto.MenuDTO;
+import com.uplog.uplog.domain.menu.model.Menu;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.uplog.uplog.domain.menu.dto.MenuDTO.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class MenuController {
+    private final MenuService menuService;
+
+    /*
+    CREATE
+     */
+    @PostMapping(value="/menus/{project-id}")
+    public ResponseEntity<MenuInfoDTO> createMenu(@PathVariable(name="project-id") Long projectId, @RequestBody CreateMenuRequest createMenuRequest){
+        Menu createMenu=menuService.createMenu(projectId,createMenuRequest);
+        MenuInfoDTO menuInfoDTO=createMenu.toMenuInfoDTO();
+        return ResponseEntity.ok(menuInfoDTO);
+    }
+
 }
--- src/main/java/com/uplog/uplog/domain/menu/application/MenuService.java
+++ src/main/java/com/uplog/uplog/domain/menu/application/MenuService.java
@@ -1,11 +1,34 @@
 package com.uplog.uplog.domain.menu.application;
 
+import com.uplog.uplog.domain.menu.dao.MenuRepository;
+import com.uplog.uplog.domain.menu.dto.MenuDTO.*;
+import com.uplog.uplog.domain.menu.model.Menu;
+import com.uplog.uplog.domain.project.dao.ProjectRepository;
+import com.uplog.uplog.domain.project.model.Project;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MenuService {
+    private final MenuRepository menuRepository;
+    private final ProjectRepository projectRepository;
+
+    /*
+    CREATE
+     */
+    @Transactional
+    public Menu createMenu(Long projectId, @RequestBody CreateMenuRequest createMenuRequest){
+        Project project=projectRepository.findById(projectId)
+                .orElseThrow(() -> new RuntimeException("Project not found"));
+
+        Menu menu=createMenuRequest.toEntity(project);
+        menuRepository.save(menu);
+        return menu;
+    }
 }
+
