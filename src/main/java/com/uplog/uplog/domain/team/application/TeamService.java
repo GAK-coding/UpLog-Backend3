cf2ec4ee498369dce299f003080dc21b04c0568c
yunjjung <yjung203@naver.com>
2023-08-06 11:18:08 +0900

    [REFACTOR]mail 이중으로 나가는거 수정

--- src/main/java/com/uplog/uplog/domain/team/application/TeamService.java
+++ src/main/java/com/uplog/uplog/domain/team/application/TeamService.java
@@ -48,16 +48,17 @@ public class TeamService {
                 .memberEmail(createTeamRequest.getMemberEmail())
                 .powerType(PowerType.MASTER)
                 .link(createTeamRequest.getLink())
+                .mailType(2)
                 .build();
         memberTeamService.createMemberTeam(createMemberTeamRequest);
-        EmailRequest emailRequest = builder()
-                .email(createMemberTeamRequest.getMemberEmail())
-                .type(2)
-                .powerType(PowerType.MASTER)
-                .link(createTeamRequest.getLink())
-                .build();
-        mailService.sendSimpleMessage(emailRequest);
-        //마스터 멤버 호출
+//        EmailRequest emailRequest = builder()
+//                .email(createMemberTeamRequest.getMemberEmail())
+//                .type(2)
+//                .powerType(PowerType.MASTER)
+//                .link(createTeamRequest.getLink())
+//                .build();
+//        mailService.sendSimpleMessage(emailRequest);
+//        //마스터 멤버 호출
 
         //TeamInfoDTO teamInfoDTO = team.toTeamInfoDTO();
         return team.getId();
