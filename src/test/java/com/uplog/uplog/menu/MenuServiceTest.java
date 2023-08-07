e30197cdc61eb343e8d3525dc8268393bd0bd753
eunryeong1 <lyungeg@gmail.com>
2023-08-07 23:19:38 +0900

    [REFACTTOR] menu testcode 이름 변경

--- src/test/java/com/uplog/uplog/menu/MenuServiceTest.java
+++ src/test/java/com/uplog/uplog/menu/MenuServiceTest.java
@@ -6,6 +6,7 @@ import com.uplog.uplog.domain.member.model.Position;
 import com.uplog.uplog.domain.menu.application.MenuService;
 import com.uplog.uplog.domain.menu.dao.MenuRepository;
 import com.uplog.uplog.domain.menu.dto.MenuDTO;
+import com.uplog.uplog.domain.menu.exception.*;
 import com.uplog.uplog.domain.menu.model.Menu;
 import com.uplog.uplog.domain.post.application.PostService;
 import com.uplog.uplog.domain.post.dao.PostRepository;
@@ -21,25 +22,18 @@ import com.uplog.uplog.domain.team.application.TeamService;
 import com.uplog.uplog.domain.team.dao.TeamRepository;
 import com.uplog.uplog.domain.team.dto.TeamDTO;
 import com.uplog.uplog.domain.team.model.Team;
+import com.uplog.uplog.global.exception.NotFoundIdException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.uplog.uplog.domain.menu.dto.MenuDTO.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @Transactional
 @SpringBootTest
@@ -152,4 +146,266 @@ public class MenuServiceTest {
 
     }
 
-}
+    @Test
+    @DisplayName("중복된 메뉴 생성 시도-실패테스트")
+    public void testCreateDuplicateMenu() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        CreateMenuRequest createMenuRequest1 = createMenuRequest();
+        menuService.createMenu(project.getId(),createMenuRequest1);
+
+        // 중복된 메뉴 이름
+        String duplicateMenuName = "menu2";
+        CreateMenuRequest createMenuRequest2 = CreateMenuRequest.builder()
+                .menuName(duplicateMenuName)
+                .build();
+
+        //when & then
+        org.junit.jupiter.api.Assertions.assertThrows(DuplicatedMenuNameInProjectException.class, () -> {
+                    MenuDTO.MenuInfoDTO menuInfoDTO = menuService.createMenu(project.getId(),createMenuRequest2);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("메뉴 생성 제한 수 초과 시도-실패테스트")
+    public void testCreateExceededMaxMenuCount() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        //15개 생성
+        int numberOfMenusToCreate = 15;
+        for (int i = 1; i <= numberOfMenusToCreate; i++) {
+            String menuName = "menu" + i;
+            CreateMenuRequest createMenuRequest2 = CreateMenuRequest.builder()
+                    .menuName(menuName)
+                    .build();
+            menuService.createMenu(project.getId(), createMenuRequest2);
+        }
+
+        // 새로운 메뉴 이름
+        String newMenuName = "menu16";
+        CreateMenuRequest createMenuRequest2 = CreateMenuRequest.builder()
+                .menuName(newMenuName)
+                .build();
+
+        //when & then
+        org.junit.jupiter.api.Assertions.assertThrows(ExceededMaxMenuCountException.class, () -> {
+                    MenuDTO.MenuInfoDTO menuInfoDTO = menuService.createMenu(project.getId(),createMenuRequest2);
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("프로젝트에 속하지 않은 메뉴 정보 조회 시도-실패테스트")
+    public void testFindMenuByIdNotBelongsToProject() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        CreateMenuRequest createMenuRequest1 = createMenuRequest();
+        menuService.createMenu(project.getId(),createMenuRequest1);
+
+        // 존재하지 않는 메뉴 ID
+        Long nonExistentMenuId = 999L;
+
+        // When & Then
+        assertThatExceptionOfType(NotFoundIdException.class)
+                .isThrownBy(() -> menuService.findMenuById(nonExistentMenuId));
+    }
+
+    @Test
+    @DisplayName("메뉴 이름 변경 성공")
+    public void testUpdateMenuNameSuccess() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        CreateMenuRequest createMenuRequest1 = createMenuRequest();
+        MenuInfoDTO menuInfoDTO=menuService.createMenu(project.getId(),createMenuRequest1);
+
+        // 변경할 메뉴 ID
+        Long menuIdToUpdate = menuInfoDTO.getId();
+        String updatedMenuName = "Updated Menu";
+
+        UpdateMenuNameRequest updateMenuNameRequest = UpdateMenuNameRequest.builder()
+                .updatemenuName(updatedMenuName)
+                .build();
+
+        // When
+        MenuInfoDTO updatedMenuInfoDTO = menuService.updateMenuName(menuIdToUpdate, updateMenuNameRequest);
+
+        // Then
+        assertThat(updatedMenuInfoDTO.getMenuName()).isEqualTo(updatedMenuName);
+    }
+
+    @Test
+    @DisplayName("결과물 메뉴 이름 변경 시도-실패테스트")
+    public void testUpdateMenuNameNotAllowedForResultsMenu() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        // 변경할 메뉴 ID (결과물 메뉴)
+        String duplicateMenuName = "결과물";
+        CreateMenuRequest createMenuRequest2 = CreateMenuRequest.builder()
+                .menuName(duplicateMenuName)
+                .build();
+        MenuInfoDTO menuInfoDTO=menuService.createMenu(project.getId(),createMenuRequest2);
+
+        Long resultsMenuId = menuInfoDTO.getId();
+        String updatedMenuName = "Updated Menu";
+
+        UpdateMenuNameRequest updateMenuNameRequest = UpdateMenuNameRequest.builder()
+                .updatemenuName(updatedMenuName)
+                .build();
+
+        // When & Then
+        assertThatExceptionOfType(MenuUpdateNotAllowedException.class)
+                .isThrownBy(() -> menuService.updateMenuName(resultsMenuId, updateMenuNameRequest));
+    }
+
+    @Test
+    @DisplayName("메뉴 삭제 성공")
+    public void testDeleteMenuSuccess() {
+        // Given
+        //멤버생성(회사,개인)
+        Member company = createCompanyRequest().toMemberEntity();
+        Member member = createMemberRequest().toMemberEntity();
+        memberRepository.save(company);
+        memberRepository.save(member);
+
+        //프로덕트정보로 팀생성(마스터만 존재하는 팀)
+        TeamDTO.CreateTeamRequest saveTeamRequest = TeamDTO.CreateTeamRequest.builder()
+                .teamName(createProductRequest().getName())
+                .memberEmail(createProductRequest().getMasterEmail())
+                .link(createProductRequest().getLink())
+                .build();
+        Team team = saveTeamRequest.toEntity();
+        teamRepository.save(team);
+
+        //프로덕트 생성
+        Product product = createProductRequest().toProductEntity(company.getName(), team);
+        productRepository.save(product);
+
+        //프로젝트생성
+        Project project = createProject().toEntity(product);
+        projectRepository.save(project);
+
+        CreateMenuRequest createMenuRequest1 = createMenuRequest();
+        MenuInfoDTO menuInfoDTO=menuService.createMenu(project.getId(),createMenuRequest1);
+
+        // 삭제할 메뉴 ID
+        Long menuIdToDelete = menuInfoDTO.getId();
+
+        // When
+        menuService.deleteMenu(menuIdToDelete);
+
+        // Then
+        assertThat(menuRepository.existsById(menuIdToDelete)).isFalse();
+    }
+
+    }
