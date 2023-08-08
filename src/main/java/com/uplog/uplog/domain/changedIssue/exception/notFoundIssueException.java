d51a9f55fce2199289b389fcebf1008facd588ea
eunryeong1 <lyungeg@gmail.com>
2023-08-08 19:29:57 +0900

    [REFACTOR] changeIssue 복구

--- src/main/java/com/uplog/uplog/domain/changedIssue/exception/notFoundIssueException.java
+++
@@ -1,7 +0,0 @@
-package com.uplog.uplog.domain.changedIssue.exception;
-
-public class notFoundIssueException extends IllegalArgumentException{
-
-        public notFoundIssueException(String m){super(m);}
-        public notFoundIssueException(Long IssueId){super("Id : "+IssueId+"해당 상세이력을 찾을 수가 없습니다.");}
-}
