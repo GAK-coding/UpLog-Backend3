cdcd7eb6731df493e9c7de8b10c3e3c9c4f2f98e
yunjjung <yjung203@naver.com>
2023-08-11 00:07:51 +0900

    [ADD]memberProduct domain add

---
+++ src/main/java/com/uplog/uplog/domain/project/model/MemberProduct.java
@@ -0,0 +1,47 @@
+package com.uplog.uplog.domain.project.model;
+
+import com.uplog.uplog.domain.member.model.Member;
+import com.uplog.uplog.domain.product.model.Product;
+import com.uplog.uplog.domain.team.model.PowerType;
+import com.uplog.uplog.domain.team.model.Team;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+//@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MemberProduct{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberProduct_id")
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private PowerType powerType;
+
+    private Long index; //for drag/drop
+
+    @Builder
+    public MemberProduct(Long id, Product product, Member member, PowerType powerType, Long index){
+        this.id = id;
+        this.product = product;
+        this.member = member;
+        this.powerType = powerType;
+        this.index = index;
+    }
+
+}
