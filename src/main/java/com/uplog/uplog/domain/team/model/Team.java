e3aafaaf1fbd0dba2576d3ee5ff22583cf610e85
yunjjung <yjung203@naver.com>
2023-08-11 00:08:18 +0900

    [REFACTOR]team domain refactor

--- src/main/java/com/uplog/uplog/domain/team/model/Team.java
+++ src/main/java/com/uplog/uplog/domain/team/model/Team.java
@@ -1,5 +1,7 @@
 package com.uplog.uplog.domain.team.model;
 
+import com.uplog.uplog.domain.project.model.Project;
+import com.uplog.uplog.domain.task.model.Task;
 import com.uplog.uplog.domain.team.dto.TeamDTO.TeamInfoDTO;
 import com.uplog.uplog.global.BaseTime;
 import lombok.*;
@@ -11,9 +13,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn
-@DiscriminatorValue("Team")
+@AllArgsConstructor
 public class Team extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,17 +23,35 @@ public class Team extends BaseTime {
     @OneToMany(mappedBy = "team")
     private List<MemberTeam> memberTeamList = new ArrayList<>();
 
+    @OneToMany(mappedBy = "team")
+    private List<Task> taskList = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parentTeam_id")
+    private Team parentTeam;
+
+    @OneToMany(mappedBy = "parentTeam")
+    private List<Team> childTeamList = new ArrayList<Team>();
+
 //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "product_id")
 //    private Product product;
 
     protected String name;
-    @Builder(builderMethodName = "teamBuilder")
-    public Team(Long id, List<MemberTeam> memberTeamList, String name){
+
+    @Builder
+    public Team(Long id, String name, Project project, Team parentTeam){
         this.id = id;
-        this.memberTeamList = memberTeamList;
+        //this.memberTeamList = memberTeamList;
         this.name = name;
+        this.project = project;
+        this.parentTeam = parentTeam;
     }
+
 
     public TeamInfoDTO toTeamInfoDTO(){
         return TeamInfoDTO.builder()
