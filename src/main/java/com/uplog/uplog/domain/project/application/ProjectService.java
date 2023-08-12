9a1570c19063a3862bbca8e739bf40a311433063
yunjjung <yjung203@naver.com>
2023-08-12 11:06:55 +0900

    [ADD]생성 로직 디비 수정에 맞게 생성

--- src/main/java/com/uplog/uplog/domain/project/application/ProjectService.java
+++ src/main/java/com/uplog/uplog/domain/project/application/ProjectService.java
@@ -3,10 +3,14 @@ package com.uplog.uplog.domain.project.application;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.uplog.uplog.domain.changedIssue.exception.NotFoundPowerByMemberException;
 import com.uplog.uplog.domain.comment.exception.MemberAuthorizedException;
+import com.uplog.uplog.domain.member.dao.MemberRepository;
+import com.uplog.uplog.domain.member.model.Member;
 import com.uplog.uplog.domain.menu.application.MenuService;
 import com.uplog.uplog.domain.menu.dto.MenuDTO;
 import com.uplog.uplog.domain.menu.dto.MenuDTO.SimpleMenuInfoDTO;
+import com.uplog.uplog.domain.product.dao.MemberProductRepository;
 import com.uplog.uplog.domain.product.dao.ProductRepository;
+import com.uplog.uplog.domain.product.model.MemberProduct;
 import com.uplog.uplog.domain.product.model.Product;
 import com.uplog.uplog.domain.project.dao.ProjectRepository;
 import com.uplog.uplog.domain.project.exception.DuplicateVersionNameException;
@@ -16,15 +20,16 @@ import com.uplog.uplog.domain.project.model.Project;
 import com.uplog.uplog.domain.project.model.ProjectStatus;
 import com.uplog.uplog.domain.project.model.QProject;
 import com.uplog.uplog.domain.team.application.MemberTeamService;
-import com.uplog.uplog.domain.team.application.ProjectTeamService;
+import com.uplog.uplog.domain.team.application.TeamService;
 import com.uplog.uplog.domain.team.dao.MemberTeamRepository;
-import com.uplog.uplog.domain.team.dao.ProjectTeamRepository;
+import com.uplog.uplog.domain.team.dao.TeamRepository;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO.CreateMemberTeamRequest;
 import com.uplog.uplog.domain.team.model.MemberTeam;
 import com.uplog.uplog.domain.team.model.PowerType;
-import com.uplog.uplog.domain.team.model.ProjectTeam;
 import com.uplog.uplog.domain.team.model.QMemberTeam;
+import com.uplog.uplog.domain.team.model.Team;
+import com.uplog.uplog.global.exception.AuthorityException;
 import com.uplog.uplog.global.exception.NotFoundIdException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -49,24 +54,31 @@ public class ProjectService {
     @PersistenceContext
     private EntityManager entityManager;
 
+    private final TeamRepository teamRepository;
     private final ProjectRepository projectRepository;
     private final ProductRepository productRepository;
     private final MemberTeamRepository memberTeamRepository;
-    private final ProjectTeamRepository projectTeamRepository;
+    private final MemberProductRepository memberProductRepository;
+    private final MemberRepository memberRepository;
 
     private final MenuService menuService;
-    private final ProjectTeamService projectTeamService;
+    private final TeamService projectTeamService;
     private final MemberTeamService memberTeamService;
 
 
     @Transactional
-    public ProjectInfoDTO createProject(CreateProjectRequest createProjectRequest, Long productId) throws Exception {
+    public ProjectInfoDTO createProject(Long memberId,CreateProjectRequest createProjectRequest, Long productId) throws Exception {
+//        Member member = memberRepository.findMemberById(memberId).orElseThrow(NotFoundIdException::new);
+        MemberProduct memberProduct = memberProductRepository.findMemberProductByMemberIdAndProductId(memberId, productId).orElseThrow(NotFoundIdException::new);
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new NotFoundProjectException(productId));
 
         //진행 중 프로젝트 있을 시 제한
         checkProcessProject(productId);
-        //TODO Member가 마스터인지 확인해야함. -> 스프링 시큐리티!
+        //Member가 마스터인,리더인지 확인해야함. -> 스프링 시큐리티! -> 이건 제품에서 확인
+        if(memberProduct.getPowerType()!= PowerType.MASTER || memberProduct.getPowerType()!= PowerType.LEADER){
+            throw new AuthorityException("프로젝트 생성 권한이 없습니다.");
+        }
 
         //새로 생성 되는 프로젝트는 진행 중 고정 값.
         Project project = createProjectRequest.toEntity(product);
@@ -75,128 +87,36 @@ public class ProjectService {
         //menu 생성
         List<SimpleMenuInfoDTO> simpleMenuInfoDTOList = menuService.createDefaultMenu(project.getId());
 
-        //생성되고 바로 전체 프로젝트팀 만들어져야함.
-        //프로덕트 멤버 가져오기
-        /*
-         private List<Long> memberIdList;
-        private String name;
-        private Long projectId;
-        private Long parentProjectTeamId;
-         */
+        //팀 생성.
         //TODO Link 부분 상의
-//        List<MemberTeam> memberTeamList = new ArrayList<>();
-//        log.info(product.getTeam().getMemberTeamList().size()+"size");
-//        MemberTeam[] memberTeams = product.getTeam().getMemberTeamList().toArray(MemberTeam[]::new);
+        Team team = Team.builder()
+                .project(project)
+                .parentTeam(null)
+                .name(createProjectRequest.getVersion())
+                .build();
+        teamRepository.save(team);
 
-//        for (int i = 0 ; i < product.getTeam().getMemberTeamList().size() ; i++ ) {
-//            MemberTeam mt = memberTeams[i];
-//            CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-//                    .memberId(mt.getMember().getId())
-//                    .teamId(project.getId())
-//                    .powerType(mt.getPowerType() == PowerType.MASTER || mt.getPowerType() == PowerType.LEADER ? PowerType.LEADER : mt.getPowerType())
-//                    .mailType(2)
-//                    .link("")
-//                    .build();
-//
-//            Long memberTeamId = memberTeamService.createMemberTeam(createMemberTeamRequest);
-//            MemberTeam memberProjectTeam = memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
-//            memberTeamList.add(memberProjectTeam);
-//            if(i==product.getTeam().getMemberTeamList().size()-1) break;
-//        }
-//        for (MemberTeam mt : product.getTeam().getMemberTeamList()) {
-//
-//            CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-//                    .memberId(mt.getMember().getId())
-//                    .teamId(project.getId())
-//                    .powerType(mt.getPowerType() == PowerType.MASTER || mt.getPowerType() == PowerType.LEADER ? PowerType.LEADER : mt.getPowerType())
-//                    .mailType(2)
-//                    .link("")
-//                    .build();
-//
-//            Long memberTeamId = memberTeamService.createMemberTeam(createMemberTeamRequest);
-//            MemberTeam memberProjectTeam = memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
-//            memberTeamList.add(memberProjectTeam);
-//        }
-//        List<MemberTeam> memberTeamList = product.getTeam().getMemberTeamList()
-//                .stream()
-//                .map(mt -> {
-//                    CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-//                            .memberId(mt.getMember().getId())
-//                            .teamId(project.getId())
-//                            .powerType(mt.getPowerType()==PowerType.MASTER||mt.getPowerType().equals(PowerType.LEADER)?
-//                                    PowerType.LEADER:mt.getPowerType())
-//                            .mailType(2)
-//                            .link("")
-//                            .build();
-//
-//                    Long memberTeamId = null;
-//                    try {
-//                        memberTeamId = memberTeamService.createMemberTeam(createMemberTeamRequest);
-//                    } catch (Exception e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                   // MemberTeam memberProjectTeam = memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
-//                    return memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
-//                })
-//                .collect(Collectors.toList());
 
-//        List<MemberTeam> memberTeamList = product.getTeam().getMemberTeamList()
-//                .stream()
-//                .map(mt -> {
-//                    CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-//                            .memberId(mt.getMember().getId())
-//                            .teamId(project.getId())
-//                            .powerType(mt.getPowerType()==PowerType.MASTER||mt.getPowerType().equals(PowerType.LEADER)?
-//                                    PowerType.LEADER:mt.getPowerType())
-//                            .mailType(2)
-//                            .link("")
-//                            .build();
-//
-//                })
-//                .collect(Collectors.toList());
-//        List<MemberTeam> memberTeamList = new ArrayList<>();
-//        for (MemberTeam mt : product.getTeam().getMemberTeamList()) {
-//            CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-//                    .memberId(mt.getMember().getId())
-//                    .teamId(project.getId())
-//                    .powerType(mt.getPowerType() == PowerType.MASTER || mt.getPowerType() == PowerType.LEADER ? PowerType.LEADER : mt.getPowerType())
-//                    .mailType(2)
-//                    .link("")
-//                    .build();
-//
-//
-//            memberTeamList.add(ㅊc);
-//        }
-
+        //현재 멤버를 넣어줘야함.
         List<MemberTeam> memberTeamList = new ArrayList<>();
-        log.info(product.getTeam().getMemberTeamList().size()+"size");
-        for (MemberTeam mt : product.getTeam().getMemberTeamList()) {
+        for (MemberProduct mp : product.getMemberProductList()) {
 
             CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-                    .memberId(mt.getMember().getId())
-                    .teamId(project.getId())
-                    .powerType(mt.getPowerType() == PowerType.MASTER || mt.getPowerType() == PowerType.LEADER ? PowerType.LEADER : mt.getPowerType())
-                    .mailType(2)
-                    .link("")
+                    .memberId(mp.getMember().getId())
+                    .teamId(team.getId())
+                    .powerType(mp.getPowerType() == PowerType.MASTER || mp.getPowerType() == PowerType.LEADER ? PowerType.LEADER : mp.getPowerType())
+                    .link(createProjectRequest.getLink())
                     .build();
 
             Long memberTeamId = memberTeamService.createMemberTeam(createMemberTeamRequest);
-            MemberTeam memberProjectTeam = memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
-            memberTeamList.add(memberProjectTeam);
+            MemberTeam memberTeam = memberTeamRepository.findMemberTeamById(memberTeamId).orElseThrow(NotFoundIdException::new);
+            team.getMemberTeamList().add(memberTeam);
         }
-        ProjectTeam projectTeam = ProjectTeam.projectTeamBuilder()
-                .project(project)
-                .parentTeam(null)
-                .memberTeamList(memberTeamList)
-                .name(createProjectRequest.getVersion())
-                .build();
-        projectTeamRepository.save(projectTeam);
 
         List<Long> projectTeamIdList = new ArrayList<>();
-        projectTeamIdList.add(projectTeam.getId());
+        projectTeamIdList.add(team.getId());
 
-        log.info(project.getProjectTeamList()+"list");
-        project.getProjectTeamList().add(projectTeam);
+        project.getTeamList().add(team);
 
 
         return project.toProjectInfoDTO(simpleMenuInfoDTOList, projectTeamIdList);
