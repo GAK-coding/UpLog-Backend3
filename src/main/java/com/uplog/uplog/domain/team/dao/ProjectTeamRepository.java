aa89ec1b9b65b95c038886ade6513d1c26a43c20
yunjjung <yjung203@naver.com>
2023-08-12 11:15:01 +0900

    [REFACTOR]디비 수정으로 주석 처리

--- src/main/java/com/uplog/uplog/domain/team/dao/ProjectTeamRepository.java
+++ src/main/java/com/uplog/uplog/domain/team/dao/ProjectTeamRepository.java
@@ -1,18 +1,18 @@
-package com.uplog.uplog.domain.team.dao;
-
-import com.uplog.uplog.domain.team.model.ProjectTeam;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface ProjectTeamRepository extends JpaRepository<ProjectTeam, Long> {
-    Optional<ProjectTeam> findById(Long projectId);
-    Optional<ProjectTeam> findByProjectIdAndName(Long id, String projectName);
-    boolean existsById(Long id);
-    List<ProjectTeam> findProjectTeamsByProjectId(Long projectId);
-    //int countProjectTeamsByProjectId()
-    List<ProjectTeam> findProjectTeamsByProjectIdAndParentTeamId(Long projectId, Long parentTeamId);
-}
+//package com.uplog.uplog.domain.team.dao;
+//
+//import com.uplog.uplog.domain.team.model.ProjectTeam;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public interface ProjectTeamRepository extends JpaRepository<ProjectTeam, Long> {
+//    Optional<ProjectTeam> findById(Long projectId);
+//    Optional<ProjectTeam> findByProjectIdAndName(Long id, String projectName);
+//    boolean existsById(Long id);
+//    List<ProjectTeam> findProjectTeamsByProjectId(Long projectId);
+//    //int countProjectTeamsByProjectId()
+//    List<ProjectTeam> findProjectTeamsByProjectIdAndParentTeamId(Long projectId, Long parentTeamId);
+//}
--- src/main/java/com/uplog/uplog/domain/team/model/ProjectTeam.java
+++ src/main/java/com/uplog/uplog/domain/team/model/ProjectTeam.java
@@ -1,41 +1,41 @@
-package com.uplog.uplog.domain.team.model;
-
-import com.uplog.uplog.domain.project.model.Project;
-import com.uplog.uplog.domain.task.model.Task;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@DiscriminatorValue("Project_Team")
-public class ProjectTeam extends Team {
-    @OneToMany(mappedBy = "projectTeam")
-    private List<Task> taskList = new ArrayList<>();
-
-    @ManyToOne
-    @JoinColumn(name = "project_id")
-    private Project project;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parentTeam_id")
-    private ProjectTeam parentTeam;
-
-    @OneToMany(mappedBy = "parentTeam")
-    private List<ProjectTeam> childTeamList = new ArrayList<ProjectTeam>();
-
-    @Builder(builderMethodName = "projectTeamBuilder")
-    public ProjectTeam(Long id, List<MemberTeam> memberTeamList, String name, Project project, ProjectTeam parentTeam){
-        super(id, memberTeamList, name);
-        this.project = project;
-        this.parentTeam = parentTeam;
-    }
-}
+//package com.uplog.uplog.domain.team.model;
+//
+//import com.uplog.uplog.domain.project.model.Project;
+//import com.uplog.uplog.domain.task.model.Task;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.SuperBuilder;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@DiscriminatorValue("Project_Team")
+//public class ProjectTeam extends Team {
+//    @OneToMany(mappedBy = "projectTeam")
+//    private List<Task> taskList = new ArrayList<>();
+//
+//    @ManyToOne
+//    @JoinColumn(name = "project_id")
+//    private Project project;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "parentTeam_id")
+//    private ProjectTeam parentTeam;
+//
+//    @OneToMany(mappedBy = "parentTeam")
+//    private List<ProjectTeam> childTeamList = new ArrayList<ProjectTeam>();
+//
+//    @Builder(builderMethodName = "projectTeamBuilder")
+//    public ProjectTeam(Long id, List<MemberTeam> memberTeamList, String name, Project project, ProjectTeam parentTeam){
+//        super(id, memberTeamList, name);
+//        this.project = project;
+//        this.parentTeam = parentTeam;
+//    }
+//}
