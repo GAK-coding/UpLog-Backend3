6cc17ae4c311992fd89ef40aa2b015311bb4133a
yunjjung <yjung203@naver.com>
2023-08-13 11:47:49 +0900

    [ADD]팀 내에 속한 멤버 출력 기능 추가

--- src/main/java/com/uplog/uplog/domain/team/api/TeamController.java
+++ src/main/java/com/uplog/uplog/domain/team/api/TeamController.java
@@ -4,14 +4,19 @@ import com.uplog.uplog.domain.member.dao.MemberRepository;
 import com.uplog.uplog.domain.team.application.TeamService;
 import com.uplog.uplog.domain.team.dto.TeamDTO;
 import com.uplog.uplog.domain.team.dto.TeamDTO.CreateTeamRequest;
+import com.uplog.uplog.domain.team.dto.memberTeamDTO;
+import com.uplog.uplog.domain.team.dto.memberTeamDTO.MemberPowerDTO;
 import com.uplog.uplog.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,13 +26,20 @@ public class TeamController {
 
     private final MemberRepository memberRepository;
 
+    //================create=================
     @PostMapping(value = "/teams")
     public ResponseEntity<Long> createTeam(CreateTeamRequest createTeamRequest) throws Exception {
         Long memberId= SecurityUtil.getCurrentUsername().flatMap(memberRepository::findOneWithAuthoritiesByEmail).get().getId();
         Long teamId = teamService.createTeam(memberId, createTeamRequest);
         return new ResponseEntity<>(teamId, HttpStatus.CREATED);
     }
-
+    //=============read==========================
+    @GetMapping(value = "/teams/{team-id}")
+    public ResponseEntity<List<MemberPowerDTO>> findMemberByTeamId(@PathVariable(name = "team-id")Long teamId){
+        List<MemberPowerDTO> memberPowerDTOList = teamService.findMembersByTeamId(teamId);
+        return ResponseEntity.ok(memberPowerDTOList);
+    }
+    //===========delete==============================
     @PostMapping(value = "/teams/{team-id}")
     public ResponseEntity<String> deleteTeam(@PathVariable(name = "team-id")Long id){
         String s = teamService.deleteTeam(id);
--- src/main/java/com/uplog/uplog/domain/team/application/TeamService.java
+++ src/main/java/com/uplog/uplog/domain/team/application/TeamService.java
@@ -9,7 +9,9 @@ import com.uplog.uplog.domain.project.model.Project;
 import com.uplog.uplog.domain.team.dao.MemberTeamRepository;
 import com.uplog.uplog.domain.team.dao.TeamRepository;
 import com.uplog.uplog.domain.team.dto.TeamDTO.CreateTeamRequest;
+import com.uplog.uplog.domain.team.dto.memberTeamDTO;
 import com.uplog.uplog.domain.team.dto.memberTeamDTO.CreateMemberTeamRequest;
+import com.uplog.uplog.domain.team.dto.memberTeamDTO.MemberPowerDTO;
 import com.uplog.uplog.domain.team.model.MemberTeam;
 import com.uplog.uplog.domain.team.model.PowerType;
 import com.uplog.uplog.domain.team.model.Team;
@@ -20,6 +22,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -42,56 +47,68 @@ public class TeamService {
         Project project = projectRepository.findById(createTeamRequest.getProjectId()).orElseThrow(NotFoundIdException::new);
         //멤버가 현재 프로젝트에 속한사람인지 확인
         Team rootTeam = teamRepository.findByProjectIdAndName(project.getId(), project.getVersion()).orElseThrow(NotFoundIdException::new);
-        if(!memberTeamRepository.existsMemberTeamByMemberIdAndTeamId(currentMemberId, rootTeam.getId())){
+        if (!memberTeamRepository.existsMemberTeamByMemberIdAndTeamId(currentMemberId, rootTeam.getId())) {
             throw new AuthorityException("팀에 속하지 않은 멤버로, 팀 생성 권한이 없습니다.");
         }
 
 //            Team parentTeam = teamRepository.findTeamById(createTeamRequest.getParentTeamId());
-            //생성 외에 부모가 널로 들어왔다면, 프로젝트 생성시에 만들어진 젠체 그룹으로 부모넣어주기
-            if(createTeamRequest.getParentTeamId() != null) {
-                Team parentTeam = teamRepository.findById(createTeamRequest.getParentTeamId()).orElseThrow(NotFoundIdException::new);
-                Team team = createTeamRequest.toEntity(project, parentTeam);
-                teamRepository.save(team);
+        //생성 외에 부모가 널로 들어왔다면, 프로젝트 생성시에 만들어진 젠체 그룹으로 부모넣어주기
+        if (createTeamRequest.getParentTeamId() != null) {
+            Team parentTeam = teamRepository.findById(createTeamRequest.getParentTeamId()).orElseThrow(NotFoundIdException::new);
+            Team team = createTeamRequest.toEntity(project, parentTeam);
+            teamRepository.save(team);
 
-                //PowerType알아내기 --> 어떻게 알아내지  -> memberProduct에서 찾자. team을 이름으로 찾고 team이랑 멤버 아이디로 해도 되는데 그럼 중복된 이름이 걸릴 수 있음.
-                for (Long memberId : createTeamRequest.getMemberIdList()) {
-                    ProductMember memberProduct = productMemberRepository.findProductMemberByMemberIdAndProductId(memberId, createTeamRequest.getProjectId()).orElseThrow(NotFoundIdException::new);
-                    CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-                            .teamId(team.getId())
-                            .memberId(memberId)
-                            .powerType(memberProduct.getPowerType()==PowerType.MASTER || memberProduct.getPowerType()==PowerType.LEADER ? PowerType.LEADER : memberProduct.getPowerType())
-                            .link(createTeamRequest.getLink())
-                            .build();
-                    memberTeamService.createMemberTeam(createMemberTeamRequest);
-                }
-                return team.getId();
+            //PowerType알아내기 --> 어떻게 알아내지  -> memberProduct에서 찾자. team을 이름으로 찾고 team이랑 멤버 아이디로 해도 되는데 그럼 중복된 이름이 걸릴 수 있음.
+            for (Long memberId : createTeamRequest.getMemberIdList()) {
+                ProductMember memberProduct = productMemberRepository.findProductMemberByMemberIdAndProductId(memberId, createTeamRequest.getProjectId()).orElseThrow(NotFoundIdException::new);
+                CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
+                        .teamId(team.getId())
+                        .memberId(memberId)
+                        .powerType(memberProduct.getPowerType() == PowerType.MASTER || memberProduct.getPowerType() == PowerType.LEADER ? PowerType.LEADER : memberProduct.getPowerType())
+                        .link(createTeamRequest.getLink())
+                        .build();
+                memberTeamService.createMemberTeam(createMemberTeamRequest);
             }
-            else{
-                Team parentTeam = teamRepository.findByProjectIdAndName(createTeamRequest.getProjectId(), project.getVersion()).orElseThrow(NotFoundIdException::new);
-                Team team = createTeamRequest.toEntity(project, parentTeam);
-                teamRepository.save(team);
+            return team.getId();
+        } else {
+            Team parentTeam = teamRepository.findByProjectIdAndName(createTeamRequest.getProjectId(), project.getVersion()).orElseThrow(NotFoundIdException::new);
+            Team team = createTeamRequest.toEntity(project, parentTeam);
+            teamRepository.save(team);
 
-                for(Long memberId : createTeamRequest.getMemberIdList()) {
-                    ProductMember memberProduct = productMemberRepository.findProductMemberByMemberIdAndProductId(memberId, createTeamRequest.getProjectId()).orElseThrow(NotFoundIdException::new);
-                    CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
-                            .teamId(team.getId())
-                            .memberId(memberId)
-                            .powerType(memberProduct.getPowerType()==PowerType.MASTER || memberProduct.getPowerType()==PowerType.LEADER ? PowerType.LEADER : memberProduct.getPowerType())
-                            .link(createTeamRequest.getLink())
-                            .build();
-                    memberTeamService.createMemberTeam(createMemberTeamRequest);
+            for (Long memberId : createTeamRequest.getMemberIdList()) {
+                ProductMember memberProduct = productMemberRepository.findProductMemberByMemberIdAndProductId(memberId, createTeamRequest.getProjectId()).orElseThrow(NotFoundIdException::new);
+                CreateMemberTeamRequest createMemberTeamRequest = CreateMemberTeamRequest.builder()
+                        .teamId(team.getId())
+                        .memberId(memberId)
+                        .powerType(memberProduct.getPowerType() == PowerType.MASTER || memberProduct.getPowerType() == PowerType.LEADER ? PowerType.LEADER : memberProduct.getPowerType())
+                        .link(createTeamRequest.getLink())
+                        .build();
+                memberTeamService.createMemberTeam(createMemberTeamRequest);
             }
-                return team.getId();
+            return team.getId();
 
         }
 
+    }
+
+    //=======================Read===========================
+    //팀에 속한 멤버 출력
+    @Transactional(readOnly = true)
+    public List<MemberPowerDTO> findMembersByTeamId(Long teamId) {
+        List<MemberTeam> memberTeamList = memberTeamRepository.findMemberTeamsByTeamId(teamId);
+
+        List<MemberPowerDTO> memberPowerDTOList = new ArrayList<>();
+        for (MemberTeam mt : memberTeamList) {
+            memberPowerDTOList.add(mt.toMemberPowerDTO());
+        }
+        return memberPowerDTOList;
     }
 
 
     //TODO DELETE는 마스터만 가능하게? 기업도 가능하게 해야해? 우선 둘다 가능하게 함. -> 이건 나중에 고려
     //근데 이게 프로덕트가 삭제되면 자동으로 삭제되는건가? -> 삭제 없음
     @Transactional
-    public String deleteTeam(Long id){
+    public String deleteTeam(Long id) {
         Team team = teamRepository.findById(id).orElseThrow(NotFoundIdException::new);
         teamRepository.delete(team);
         return "DELETE";
